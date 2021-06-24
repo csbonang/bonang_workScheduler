826d91ba0c09568787ed39290135013859e07305
csbonang <csgbonang@gmail.com>
2021-06-23 21:17:44 -0700

    completed color logic for past, present, and future

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 
 //=====Current Day==============
-// currentDay: displays the current day and month 
+// today: displays the current day and month 
 var today = $( "#currentDay" ).text( moment().format('dddd, MMMM Do')) 
 
 
 //====Time blocks============== 
+    // current Hour: holds the current hour
     var currentHour = moment().hours()
+    console.log('current Hour', currentHour); 
 
     //=======About the for loop=============================================================== 
         // Retrieves the data from local storage. 
         // data is associated with a #. 
         // Each # represents time in military time (9:00 - 18:00 hours)
         // Then, we can access the value associated with each id 
-        // recall (refer to index.html). the id's for each time block are formatted as #-data 
+        // recall (refer to index.html), the id's for each time block are formatted as #-data 
     //======================================================================================== 
-    for(var i = 9; i < 18; i++)
+    for(var i = 9; i < 24; i++)
     {
         // split the id on the character and access the data from the id 
         var retriveplan = localStorage.getItem(i); 
         $(`#${i}-data`).val(retriveplan)
-        // TODO: other conditions 
+        // check if each time block is the current, past, or future time. 
+        // the colors associated with each class is displayed 
         if(i<currentHour) {
-            $(`#${i}-data`).addClass("past")
+            $(`#${i}-data`).addClass("past"); 
+        }
+        else if (i == currentHour)
+        {
+            $(`#${i}-data`).addClass("present"); 
+        }
+        else if (i > currentHour)
+        {
+            $(`#${i}-data`).addClass("future"); 
         }
     }
 
-    // once user clicks save plan, it stores the edits to local storage. 
+    // once the user clicks save plan, it stores the edits to local storage. 
     $(".saveplan").on("click",function(){
         var time = $(this).attr("id").split("-")[0]
         var userplan = $("#"+time+"-data").val()
